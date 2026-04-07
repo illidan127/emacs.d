e18a251ce031c77f7bc7f9bba724d1edaf7c6d62
illidan127 <illidan127@qq.com>
2026-04-07 21:23:24 +0800

    [配置]: 修复c/c++启用lsp和treesit-indent时冲突问题

--- eon-lisp/eon-c-c++.el
+++ eon-lisp/eon-c-c++.el
@@ -2,19 +2,6 @@
 
 ;;; c/c++ 代码编辑配置
 
-(defun eon-c++-indent-style ()
-  "c++缩进样式"
-  `(,@(alist-get 'bsd (c-ts-mode--indent-styles 'cpp))))
-
-(defun eon-c-c++-mode-init ()
-  "c/c++模式预置一些配置"
-  (setq-local treesit-indent-function (lambda (node parent bol &rest args)
-					;; debug point
-					(let ((result (treesit-simple-indent node parent bol)))
-					  ;; debug point
-					  result)))
-  (indent-tabs-mode -1))
-
 (use-package
   clang-format
   :defer t
@@ -35,14 +22,12 @@
   :config
   ;; 默认缩 进 为4空格
   (setq c-ts-mode-indent-offset 4)
-  (setq c-ts-mode-indent-style #'eon-c++-indent-style)
+  (setq lsp-enable-on-type-formatting nil)
   :hook
-  (c++-ts-mode . eon-c-c++-mode-init)
   (c++-ts-mode . electric-pair-mode)
   (c++-ts-mode . yas-minor-mode)
   (c++-ts-mode . lsp-deferred)
   (c++-ts-mode . treesit-fold-mode)
-  (c-ts-mode . eon-c-c++-mode-init)
   (c-ts-mode . electric-pair-mode)
   (c-ts-mode . yas-minor-mode)
   (c-ts-mode . lsp-deferred)
