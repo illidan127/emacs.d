9d6f040470c447d59a32a5f1ee80f6a130892f8d
illidan127 <illidan127@qq.com>
2026-07-13 10:57:07 +0800

    [配置]: 去除cursor goose agent配置

--- eon-lisp/eon-program.el
+++ eon-lisp/eon-program.el
@@ -79,13 +79,8 @@
 
 (use-package agent-shell
   :config
-  (setq agent-shell-cursor-acp-command `("cursor-agent" "--api-key" ,eon-cursor-api-key "acp"))
   (setq agent-shell-anthropic-claude-environment
         (apply #'agent-shell-make-environment-variables claude-env))
-  (setq agent-shell-goose-environment
-	(apply #'agent-shell-make-environment-variables goose-env))
-  (setq agent-shell-goose-authentication
-        (agent-shell-make-goose-authentication :none t))
   )
 
 (use-package ghostel
